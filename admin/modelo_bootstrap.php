8c27c6923265d6ec2ff555b18086632bf610b9b6
Leo Oliveira <leo2281999jose@gmail.com>
2025-11-27 21:35:22 -0300

    renamed:    admin/modelo_bootstrap.html -> admin/modelo_bootstrap.php 	new file:   css/meu_estilo.css 	new file:   menu_publico.php 	modified:   modelo_bootstrap.html

--- admin/modelo_bootstrap.php
+++ admin/modelo_bootstrap.php
@@ -5,14 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap 5.3 Offline + Font + Ícones</title>
 
+    <!-- CSS específico -->
+    <link rel="stylesheet" href="../css/meu_estilo.css">
+
     <!-- Fonte local -->
-    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="../css/fonts.css">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
 
     <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/bootstrap-icons.css">
 </head>
 <body>
 
@@ -34,6 +37,6 @@
     </div>
 
     <!-- Bootstrap JS -->
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
