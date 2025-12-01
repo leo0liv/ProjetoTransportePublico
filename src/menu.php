c1500a20c159a1a7132a6e9fb027e48a5da2f68c
Leo Oliveira <leo2281999jose@gmail.com>
2025-12-01 20:15:16 -0300

    modified:   src/menu.php 	new file:   src/page_inicial.php

--- src/menu.php
+++ src/menu.php
@@ -31,13 +31,13 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Mapa</a>
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-geo-alt"></i>&nbsp;Mapa</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Linhas</a>
+                <a class="nav-link" href="#"><i class="bi bi-signpost-2"></i>&nbsp;Linhas</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Previsões</a>
+                <a class="nav-link" href="#"><i class="bi bi-clock"></i>&nbsp;Previsões</a>
               </li>
             </ul>
             <div class="d-flex">
---
+++ src/page_inicial.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicial</title>
+
+    <!-- CSS específico -->
+    <link rel="stylesheet" href="../css/meu_estilo.css">
+
+    <!-- Fonte local -->
+    <link rel="stylesheet" href="../css/fonts.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="../css/bootstrap-icons.css">
+</head>
+<body>
+    <main class="container py-4">
+        <h1 class="mb-4">
+            <i class="bi bi-speedometer2"></i> Monitoramento em Tempo Real
+        </h1>
+    </main>
+    
+
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
